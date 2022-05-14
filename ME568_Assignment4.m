2c7ef950bc6230b468f7f0216a7016d121e9bcd1
warrenau <77596963+warrenau@users.noreply.github.com>
2022-05-13 22:44:06 -0700

    added second function to handling looping over z values. script runs. --AMW

--- ME568_Assignment4.m
+++ ME568_Assignment4.m
@@ -8,12 +8,6 @@ load dns_data.mat
 % determine average and fluctuating values; average over x for one z
 % coordiate at set time and y values
 % we can write a function that will do this for any parameter we choose
-function [U, u_prime] = ReynoldsDecomp(u)
-    % u should come in as a vector (one z value, all x values)
-    % external loop should loop over z values as needed
-    U = mean(u);
-    u_prime = u - U;
-end
 
 
 % loop set up
@@ -32,14 +26,36 @@ w = squeeze(dat.w(:,y_ind,:));
 % loop
 numz = dat.nz;
 numx = dat.nx;
-U = zeros(numz,1);
-u_prime = zeros(numz,numx);
 
-for i = 1:numz
-    [U(i),u_prime(i)] = ReynoldsDecomp(u);
+% u
+[U, u_prime] = ReynoldsLoop(u,numz,numx);
+
+% v
+[V, v_prime] = ReynoldsLoop(v,numz,numx);
+
+% w
+[W, w_prime] = ReynoldsLoop(w,numz,numx);
 
 
 
 
 
+% Reynolds decomp function
+function [U, u_prime] = ReynoldsDecomp(u)
+    % u should come in as a vector (one z value, all x values)
+    % external loop should loop over z values as needed
+    U = mean(u);
+    u_prime = u - U;
 end
+%
+
+% make another function to do the looping
+function [U, u_prime] = ReynoldsLoop(u,numz,numx)
+    U = zeros(numz,1);
+    u_prime = zeros(numz,numx);
+    for i = 1:numz
+        [U(i), u_prime(i,:)] = ReynoldsDecomp(u(:,i));
+    end
+end
+%
+
